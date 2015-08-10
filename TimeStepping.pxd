31f6faeb62608131a132548877b9b400d4efe2b7
Kyle Pressel <presselk@ethz.ch>
2015-08-10 10:06:15 +0200

    Added dynamic time stepping.

--- TimeStepping.pxd
+++ TimeStepping.pxd
@@ -6,6 +6,11 @@ cdef class TimeStepping:
     cdef:
         public double dt
         public double t
+        public double cfl_max
+        public double cfl_limit
+        public double dt_max
+        public double dt_initial
+        public double t_max
 
         double [:,:] value_copies
         double [:,:] tendency_copies
@@ -14,13 +19,19 @@ cdef class TimeStepping:
         public int n_rk_steps
         public int ts_type
 
+
+
         void initialize_second(self,PrognosticVariables.PrognosticVariables PV)
         void initialize_third(self,PrognosticVariables.PrognosticVariables PV)
 
+
     cpdef initialize(self, namelist, PrognosticVariables.PrognosticVariables PV, ParallelMPI.ParallelMPI Pa)
 
-    cpdef update(self,Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV)
+    cpdef update(self,Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV, ParallelMPI.ParallelMPI Pa)
 
     cpdef update_second(self,Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV)
     cpdef update_third(self,Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV)
 
+    cdef void compute_cfl_max(self,Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV, ParallelMPI.ParallelMPI Pa)
+
+    cdef inline double cfl_time_step(self)
--- TimeStepping.pyx
+++ TimeStepping.pyx
@@ -1,16 +1,19 @@
 cimport ParallelMPI as ParallelMPI
 cimport PrognosticVariables as PrognosticVariables
 cimport Grid as Grid
+cimport mpi4py.mpi_c as mpi
 
 import numpy as np
 cimport numpy as np
 
 import cython
+from libc.math cimport fmin, fmax, fabs
 
 cdef class TimeStepping:
 
     def __init__(self):
-        pass
+
+        return
 
 
     cpdef initialize(self,namelist,PrognosticVariables.PrognosticVariables PV, ParallelMPI.ParallelMPI Pa):
@@ -23,13 +26,17 @@ cdef class TimeStepping:
             Pa.root_print('Killing simulation now')
             Pa.kill()
 
-
         try:
-            self.dt = namelist['time_stepping']['dti']
+            self.dt = namelist['time_stepping']['dt_initial']
         except:
-            Pa.root_print('dti  (initial time step) not given in namelist so taking defualt value dti = 1.0')
+            Pa.root_print('dt_initial (initial time step) not given in namelist so taking defualt value dt_initail = 1.0')
             self.dt = 1.0
 
+        try:
+            self.dt_max = namelist['time_stepping']['dt_max']
+        except:
+            Pa.root_print('dt_max (maximum permissible time step) not given in namelist so taking default value dt_max =10.0')
+            self.dt_max = 10.0
 
         try:
             self.t = namelist['time_stepping']['t']
@@ -37,6 +44,17 @@ cdef class TimeStepping:
             Pa.root_print('t (initial time) not given in namelist so taking default value t = 0')
             self.t = 0.0
 
+        try:
+            self.cfl_limit = namelist['time_stepping']['cfl_limit']
+        except:
+            Pa.root_print('cfl_limit (maximum permissible cfl number) not given in namelist so taking default value cfl_max=0.7')
+            self.cfl_limit = 0.7
+
+        try:
+            self.t_max = namelist['time_stepping']['t_max']
+        except:
+            Pa.root_print('t_max (time at end of simulation) not given in name list! Killing Simulation Now')
+            Pa.kill()
 
 
         #Now initialize the correct time stepping routine
@@ -56,11 +74,21 @@ cdef class TimeStepping:
     @cython.boundscheck(False)  #Turn off numpy array index bounds checking
     @cython.wraparound(False)   #Turn off numpy array wrap around indexing
     @cython.cdivision(True)
-    cpdef update(self, Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV):
+    cpdef update(self, Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV, ParallelMPI.ParallelMPI Pa):
+
         if self.ts_type == 2:
             self.update_second(Gr,PV)
-        if self.ts_type == 3:
+        elif self.ts_type == 3:
             self.update_third(Gr,PV)
+
+        #Compute the CFL number and diffusive stability criterion
+        if self.rk_step == self.n_rk_steps - 1:
+            self.compute_cfl_max(Gr, PV, Pa)
+            self.dt = self.cfl_time_step()
+
+            #Diffusive limiting not yet implemented
+            if self.t + self.dt > self.t_max:
+                self.dt = self.t_max - self.t
 
 
         return
@@ -83,6 +111,7 @@ cdef class TimeStepping:
             for i in xrange(Gr.dims.npg*PV.nv):
                 PV.values[i] = 0.5 * (self.value_copies[0,i] + PV.values[i] + PV.tendencies[i] * self.dt)
                 PV.tendencies[i] = 0.0
+            self.t += self.dt
 
         return
 
@@ -108,7 +137,10 @@ cdef class TimeStepping:
             for i in xrange(Gr.dims.npg*PV.nv):
                 PV.values[i] = (1.0/3.0) * self.value_copies[0,i] + (2.0/3.0)*(PV.values[i] + PV.tendencies[i]*self.dt)
                 PV.tendencies[i] = 0.0
+            self.t += self.dt
+
         return
+
 
 
     cdef void initialize_second(self,PrognosticVariables.PrognosticVariables PV):
@@ -133,3 +165,53 @@ cdef class TimeStepping:
         self.tendency_copies = None
 
         return
+
+    @cython.boundscheck(False)  #Turn off numpy array index bounds checking
+    @cython.wraparound(False)   #Turn off numpy array wrap around indexing
+    @cython.cdivision(True)
+    cdef void compute_cfl_max(self,Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV, ParallelMPI.ParallelMPI Pa):
+
+        cdef:
+            double cfl_max_local = -9999.0
+            double [3] dxi = Gr.dims.dxi
+            long u_shift = PV.get_varshift(Gr,'u')
+            long v_shift = PV.get_varshift(Gr,'v')
+            long w_shift = PV.get_varshift(Gr,'w')
+
+            long imin = Gr.dims.gw
+            long jmin = Gr.dims.gw
+            long kmin = Gr.dims.gw
+
+            long imax = Gr.dims.nlg[0] - Gr.dims.gw
+            long jmax = Gr.dims.nlg[1] - Gr.dims.gw
+            long kmax = Gr.dims.nlg[2] - Gr.dims.gw
+
+            long istride = Gr.dims.nlg[1] * Gr.dims.nlg[2]
+            long jstride = Gr.dims.nlg[1]
+            long i,j,k, ijk, ishift, jshift
+
+        with nogil:
+            for i in xrange(imin,imax):
+                ishift = i * istride
+                for j in xrange(jmin,jmax):
+                    jshift = j * jstride
+                    for k in xrange(kmin,kmax):
+                        ijk = ishift + jshift + k
+                        cfl_max_local = fmax(cfl_max_local, self.dt * (fabs(PV.values[u_shift + ijk])*dxi[0] + fabs(PV.values[v_shift+ijk])*dxi[1] + fabs(PV.values[w_shift+ijk])*dxi[2]))
+
+
+
+        mpi.MPI_Allreduce(&cfl_max_local,&self.cfl_max,1,
+                          mpi.MPI_DOUBLE,mpi.MPI_MAX,Pa.comm_world)
+
+        self.cfl_max += 1e-11
+
+
+        return
+
+
+    cdef inline double cfl_time_step(self):
+        cdef double exact = fmin(self.dt_max,self.cfl_limit/(self.cfl_max/self.dt))
+        return fmin(exact, self.dt_max)
+
+
