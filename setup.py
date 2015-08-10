8f38c90508e3f9c42abb2a3cb110359367a10963
Kyle Pressel <presselk@ethz.ch>
2015-08-10 11:42:36 +0200

    Removed extra linespace from setup.py

--- setup.py
+++ setup.py
@@ -144,7 +144,6 @@ _ext = Extension("TimeStepping",["TimeStepping.pyx"],include_dirs = include_path
                  runtime_library_dirs=library_dirs  )
 extensions.append(_ext)
 
-
 _ext = Extension("Kinematics",["Kinematics.pyx"],include_dirs = include_path,
                  extra_compile_args=extra_compile_args, libraries=libraries , library_dirs=library_dirs,
                  runtime_library_dirs=library_dirs  )
