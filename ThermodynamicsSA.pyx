b22eca6932b4df8be889f3af9e1dee22da9d53f4
Kyle Pressel <presselk@ethz.ch>
2015-08-11 12:20:52 +0200

    Added output of 3d entropy potential temperature fields to saturation adjustment thermodynamics.

--- ThermodynamicsSA.pyx
+++ ThermodynamicsSA.pyx
@@ -7,6 +7,8 @@ cimport Grid
 cimport ReferenceState
 cimport DiagnosticVariables
 from Thermodynamics cimport LatentHeat, ClausiusClapeyron
+from thermodynamic_functions cimport thetas_c
+import cython
 
 cdef extern from "thermodynamics_sa.h":
     inline double alpha_c(double p0, double T, double qt, double qv) nogil
@@ -105,7 +107,38 @@ cdef class ThermodynamicsSA:
         cdef double lam = self.Lambda_fp(t)
         return self.L_fp(lam,t)
 
+    @cython.boundscheck(False)  #Turn off numpy array index bounds checking
+    @cython.wraparound(False)   #Turn off numpy array wrap around indexing
+    @cython.cdivision(True)
     cpdef write_fields(self, Grid.Grid Gr, ReferenceState.ReferenceState RS,
                  PrognosticVariables.PrognosticVariables PV, DiagnosticVariables.DiagnosticVariables DV, NetCDFIO_Fields NF, ParallelMPI.ParallelMPI Pa):
 
+        cdef:
+            long i,j,k, ijk, ishift, jshift
+            long istride = Gr.dims.nlg[1] * Gr.dims.nlg[2]
+            long jstride = Gr.dims.nlg[2]
+            long imin = Gr.dims.gw
+            long jmin = Gr.dims.gw
+            long kmin = Gr.dims.gw
+            long imax = Gr.dims.nlg[0] - Gr.dims.gw
+            long jmax = Gr.dims.nlg[1] - Gr.dims.gw
+            long kmax = Gr.dims.nlg[2] - Gr.dims.gw
+            long count
+            long s_shift = PV.get_varshift(Gr,'s')
+            long qt_shift = PV.get_varshift(Gr,'qt')
+            double [:] data = np.empty((Gr.dims.npl,),dtype=np.double,order='c')
+
+        #Add entropy potential temperature to 3d fields
+        with nogil:
+            count = 0
+            for i in range(imin,imax):
+                ishift = i * istride
+                for j in range(jmin,jmax):
+                    jshift = j * jstride
+                    for k in range(kmin,kmax):
+                        ijk = ishift + jshift + k
+                        data[count] = thetas_c(PV.values[s_shift+ijk],PV.values[qt_shift+ijk])
+                        count += 1
+        NF.add_field('thetas')
+        NF.write_field('thetas',data)
         return
