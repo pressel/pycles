4bdffcbd87596b86876b30a85ae3363ccebf26a8
Kyle Pressel <presselk@ethz.ch>
2015-08-19 09:30:18 +0200

    Added files for damping and included Forcing and Damping extensions in setup.py.

--- setup.py
+++ setup.py
@@ -164,6 +164,16 @@ _ext = Extension("NetCDFIO",["NetCDFIO.pyx"],include_dirs = include_path,
                  runtime_library_dirs=library_dirs  )
 extensions.append(_ext)
 
+_ext = Extension("Forcing",["Forcing.pyx"],include_dirs = include_path,
+                 extra_compile_args=extra_compile_args, libraries=libraries , library_dirs=library_dirs,
+                 runtime_library_dirs=library_dirs  )
+extensions.append(_ext)
+
+
+_ext = Extension("Damping",["Damping.pyx"],include_dirs = include_path,
+                 extra_compile_args=extra_compile_args, libraries=libraries , library_dirs=library_dirs,
+                 runtime_library_dirs=library_dirs  )
+extensions.append(_ext)
 
 setup(
     ext_modules = cythonize(extensions,verbose=1,include_path=include_path)
