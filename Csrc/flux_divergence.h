8c67370fa0ef1d4b8d075c8eb706467c049b9ffc
Kyle Pressel <presselk@ethz.ch>
2015-08-26 13:16:04 +0200

    Flux divergence computation working in C.

--- Csrc/flux_divergence.h
+++ Csrc/flux_divergence.h
@@ -4,14 +4,77 @@
 void scalar_flux_divergence(struct DimStruct *dims, double *alpha0, double *alpha0_half, double *flux, double *tendency,
     double dx, size_t d){
 
-    size_t imin = dims->gw;
-    size_t jmin = dims->gw;
-    size_t kmin = dims->gw;
+    const size_t imin = dims->gw;
+    const size_t jmin = dims->gw;
+    const size_t kmin = dims->gw;
 
-    size_t imax = dims->nlg[0] - dims->gw;
-    size_t jmax = dims->nlg[1] - dims->gw;
-    size_t kmax = dims->nlg[2] - dims->gw;
+    const size_t imax = dims->nlg[0] - dims->gw;
+    const size_t jmax = dims->nlg[1] - dims->gw;
+    const size_t kmax = dims->nlg[2] - dims->gw;
 
+    const size_t istride = dims->nlg[1] * dims->nlg[2];
+    const size_t jstride = dims->nlg[2];
+    const double dxi = 1.0/dx;
 
+    const size_t stencil[3] = {istride,jstride,1};
+    const size_t sm1 = -stencil[d];
 
+    for(size_t i=imin; i<imax; i++){
+        const size_t ishift = i * istride;
+        for(size_t j=jmin; j<jmax; j++){
+            const size_t jshift = j * jstride;
+            for(size_t k=kmin; k<kmax; k++){
+                const size_t ijk = ishift + jshift + k;
+                tendency[ijk] -= alpha0_half[k] * (flux[ijk] - flux[ijk + sm1])*dxi;
+            } // End k loop
+        } // End j loop
+    } // End i loop
 }
+
+void momentum_flux_divergence(struct DimStruct *dims, double *alpha0, double *alpha0_half, double *flux,
+                                double *tendency, size_t d_advected, size_t d_advecting){
+
+    const size_t imin = dims->gw;
+    const size_t jmin = dims->gw;
+    const size_t kmin = dims->gw;
+
+    const size_t imax = dims->nlg[0] - dims->gw;
+    const size_t jmax = dims->nlg[1] - dims->gw;
+    const size_t kmax = dims->nlg[2] - dims->gw;
+
+    const size_t istride = dims->nlg[1] * dims->nlg[2];
+    const size_t jstride = dims->nlg[2];
+    const double dxi = 1.0/dims->dx[d_advecting];
+
+    const size_t stencil[3] = {istride,jstride,1};
+    const size_t sm1 = -stencil[d_advecting];
+
+    if(d_advected != 2){
+        for(size_t i=imin; i<imax; i++){
+            const size_t ishift = i * istride;
+            for(size_t j=jmin; j<jmax; j++){
+                const size_t jshift = j * jstride;
+                for(size_t k=kmin; k<kmax; k++){
+                    const size_t ijk = ishift + jshift + k;
+                    tendency[ijk] -= alpha0_half[k] * (flux[ijk] - flux[ijk + sm1])*dxi;
+                } // End k loop
+            } // End j loop
+        } // End i loop
+
+    } // End if
+    else{
+        for(size_t i=imin; i<imax; i++){
+            const size_t ishift = i * istride;
+            for(size_t j=jmin; j<jmax; j++){
+                const size_t jshift = j * jstride;
+                for(size_t k=kmin; k<kmax; k++){
+                    const size_t ijk = ishift + jshift + k;
+                    tendency[ijk] -= alpha0[k] * (flux[ijk] - flux[ijk + sm1])*dxi;
+                } // End k loop
+            } // End j loop
+        } // End i loop
+    } // End else
+
+
+
+    }
