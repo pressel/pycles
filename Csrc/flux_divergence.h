824c7bf6157af7b1cd92efb7e6f6772ea48a03d5
Kyle Pressel <presselk@ethz.ch>
2015-08-26 12:54:10 +0200

    Begin adding source code for scalar_flux_divergence in flux_divergence.h.

--- Csrc/flux_divergence.h
+++ Csrc/flux_divergence.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "grid.h"
+
+void scalar_flux_divergence(struct DimStruct *dims, double *alpha0, double *alpha0_half, double *flux, double *tendency,
+    double dx, size_t d){
+
+    size_t imin = dims->gw;
+    size_t jmin = dims->gw;
+    size_t kmin = dims->gw;
+
+    size_t imax = dims->nlg[0] - dims->gw;
+    size_t jmax = dims->nlg[1] - dims->gw;
+    size_t kmax = dims->nlg[2] - dims->gw;
+
+
+
+}
