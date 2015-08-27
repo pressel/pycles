02c32b036417786457591eb71c896db495fc064c
Kyle Pressel <presselk@ethz.ch>
2015-08-27 15:28:38 +0200

    Cleaned up entropies.

--- entropies.pyx
+++ entropies.pyx
@@ -1,6 +1,6 @@
 cpdef inline double sd(double pd, double T) nogil:
-    return sd_c(pd,T)
+    return sd_c(pd, T)
 cpdef inline double sv(double pv, double T) nogil:
-    return sv_c(pv,T)
+    return sv_c(pv, T)
 cpdef inline double sc(double L, double T) nogil:
     return sc_c(L, T)
