798ce154f38f3416516481ca26d1065b052044ef
Kyle Pressel <presselk@ethz.ch>
2015-08-28 10:16:36 +0200

    Undo some weird formatting issues. No change in functionality.

--- Kinematics.pxd
+++ Kinematics.pxd
@@ -2,9 +2,9 @@ cimport Grid
 cimport PrognosticVariables
 cdef class Kinematics:
     cdef:
-        double [:] vgrad
-        double [:] strain_rate
-        double [:] strain_rate_mag
+        double[:] vgrad
+        double[:] strain_rate
+        double[:] strain_rate_mag
         Py_ssize_t get_grad_shift(self, Grid.Grid Gr, Py_ssize_t vel_i, Py_ssize_t dx_j)
     cpdef initialize(self, Grid.Grid Gr)
     cpdef update(self, Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV)
--- Lookup.pyx
+++ Lookup.pyx
@@ -8,18 +8,18 @@ cdef class Lookup:
 
     cpdef initialize(self, double[:] x, double[:] y):
         cdef long n = np.shape(y)[0]
-        init_table( & self.LookupStructC, n, & x[0], & y[0])
+        init_table( &self.LookupStructC, n, &x[0], &y[0])
         return
 
     cpdef finalize(self):
-        free_table(& self.LookupStructC)
+        free_table(&self.LookupStructC)
         return
 
     cpdef table_bounds(self):
         return self.LookupStructC.x_min, self.LookupStructC.x_max, self.LookupStructC.y_min, self.LookupStructC.y_max
 
     cpdef lookup(self, double x):
-        return lookup( & self.LookupStructC, x)
+        return lookup(&self.LookupStructC, x)
 
     cdef inline double fast_lookup(self, double x) nogil:
-        return lookup( & self.LookupStructC, x)
+        return lookup(&self.LookupStructC, x)
