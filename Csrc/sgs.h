88d721c8a3bafc83c8ef378e34700d5804534cb5
cmkaul <cmkaul@gmail.com>
2015-09-03 15:54:08 +0200

    bug fix in smagorinsky_update

--- Csrc/sgs.h
+++ Csrc/sgs.h
@@ -1,4 +1,3 @@
-
 void smagorinsky_update(const struct DimStruct *dims, double* restrict visc, double* restrict diff,
 double* restrict buoy_freq, double* restrict strain_rate_mag, double cs, double prt){
 
