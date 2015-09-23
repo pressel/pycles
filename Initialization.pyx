4f22403047ae34d69d4d7bcc86c8368a714f4852
Kyle Pressel <presselk@ethz.ch>
2015-09-23 10:20:46 +0200

    Added initial perturbations for DYCOMS below 840m.

--- Initialization.pyx
+++ Initialization.pyx
@@ -520,7 +520,7 @@ def InitDYCOMS_RF01(Grid.Grid Gr,PrognosticVariables.PrognosticVariables PV,
                 PV.values[ijk + qt_varshift]  = qt[k]
 
                 #Now set the entropy prognostic variable including a potential temperature perturbation
-                if Gr.zl_half[k] < 200.0:
+                if Gr.zl_half[k] < 840.0:
                     theta_pert_ = (theta_pert[ijk] - 0.5)* 0.1
                 else:
                     theta_pert_ = 0.0
