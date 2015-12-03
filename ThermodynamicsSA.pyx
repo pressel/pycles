ab0fecab1e97e5628e2801da886d43ab2c71eeeb
Kyle Pressel <presselk@ethz.ch>
2015-12-03 16:07:54 +0100

    Wrapped clip_qt function in Cython.

--- ThermodynamicsSA.pyx
+++ ThermodynamicsSA.pyx
@@ -20,12 +20,13 @@ from libc.math cimport fmax, fmin
 
 cdef extern from "thermodynamics_sa.h":
     inline double alpha_c(double p0, double T, double qt, double qv) nogil
-    void eos_c(Lookup.LookupStruct * LT, double(*lam_fp)(double), double(*L_fp)(double, double), double p0, double s, double qt, double * T, double * qv, double * ql, double * qi) nogil
-    void eos_update(Grid.DimStruct * dims, Lookup.LookupStruct * LT, double(*lam_fp)(double), double(*L_fp)(double, double), double * p0, double * s, double * qt, double * T,
+    void eos_c(Lookup.LookupStruct *LT, double(*lam_fp)(double), double(*L_fp)(double, double), double p0, double s, double qt, double *T, double *qv, double *ql, double *qi) nogil
+    void eos_update(Grid.DimStruct *dims, Lookup.LookupStruct *LT, double(*lam_fp)(double), double(*L_fp)(double, double), double *p0, double *s, double *qt, double *T,
                     double * qv, double * ql, double * qi, double * alpha)
-    void buoyancy_update_sa(Grid.DimStruct * dims, double * alpha0, double * alpha, double * buoyancy, double * wt)
-    void bvf_sa(Grid.DimStruct * dims, Lookup.LookupStruct * LT, double(*lam_fp)(double), double(*L_fp)(double, double), double * p0, double * T, double * qt, double * qv, double * theta_rho, double * bvf)
-    void thetali_update(Grid.DimStruct *dims, double (*lam_fp)(double), double (*L_fp)(double, double), double*  p0, double*  T, double*  qt, double*  ql, double*  qi, double*  thetali)
+    void buoyancy_update_sa(Grid.DimStruct *dims, double *alpha0, double *alpha, double *buoyancy, double *wt)
+    void bvf_sa(Grid.DimStruct * dims, Lookup.LookupStruct * LT, double(*lam_fp)(double), double(*L_fp)(double, double), double *p0, double *T, double *qt, double *qv, double *theta_rho, double *bvf)
+    void thetali_update(Grid.DimStruct *dims, double (*lam_fp)(double), double (*L_fp)(double, double), double *p0, double *T, double *qt, double *ql, double *qi, double *thetali)
+    void clip_qt(Grid.DimStruct *dims, double  *qt, double clip_value)
 
 cdef extern from "thermodynamic_functions.h":
     # Dry air partial pressure
