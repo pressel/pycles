673e1f045354bf254dd46dd101846578b83b1d6c
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2016-03-18 19:42:24 +0100

    adding 4th order ql schemes (mom finished, scal started)

--- Csrc/momentum_advection_central_ql.h
+++ Csrc/momentum_advection_central_ql.h
@@ -7,6 +7,7 @@
 #include<stdio.h>
 
 // !!!!!!!! Need to Fix Flux computation (subtracting eddy flux) !!!!!
+// !!!!!!!! need to be able to output eddy_flux???
 
 // QUESTIONS / NOTES:
 // need to be able to output eddy_flux???
@@ -55,7 +56,6 @@ void second_order_m_ql(struct DimStruct *dims, double* restrict rho0, double* re
         const ssize_t kmax = dims->nlg[2]-1;
 
         const ssize_t stencil[3] = {istride,jstride,1};             // array, containing 3 elements
-        // ??? ed vs ing
         const ssize_t sp1_ed = stencil[d_advecting];
         const ssize_t sp1_ing = stencil[d_advected];                // d_advecting, d_advected: given as parameters to this function
 
@@ -171,6 +171,146 @@ void second_order_m_ql(struct DimStruct *dims, double* restrict rho0, double* re
 
         //Free dynamically allocated array
 
+        free(eddy_flux);
+        free(mean_eddy_flux);
+        free(vel_int_ing);
+        free(vel_int_ed);
+        free(vel_mean_ed);
+        free(vel_mean_ing);     // gives error message
+
+        free(flux);
+        return;
+    }
+
+
+
+
+
+void fourth_order_m_ql(struct DimStruct *dims, double* restrict rho0, double* restrict rho0_half,
+    double* restrict alpha0, double* restrict alpha0_half,
+    double* restrict vel_advected, double* restrict vel_advecting,
+    double* restrict tendency, ssize_t d_advected, ssize_t d_advecting){
+
+        if (d_advected==1 && d_advecting==1){
+            printf("2nd order QL Momentum Transport \n");}
+
+        // Dynamically allocate flux array
+        double *flux = (double *)malloc(sizeof(double)*dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);     // malloc allocates size of uninitialized storage; in this case allocates memory for (nlg[0]+nlg[1]+nlg[2])*sizeof(double))
+        double *eddy_flux = (double *)malloc(sizeof(double)*dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
+        double *mean_eddy_flux = (double *)malloc(sizeof(double) * dims->nlg[2]);
+        double *vel_int_ed = (double *)malloc(sizeof(double)*dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
+        double *vel_int_ing = (double *)malloc(sizeof(double)*dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
+        // double *vel_fluc = (double *)malloc(sizeof(double)*dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
+        double *vel_mean_ed = (double *)malloc(sizeof(double) * dims->nlg[2]);
+        double *vel_mean_ing = (double *)malloc(sizeof(double) * dims->nlg[2]);
+
+        const ssize_t istride = dims->nlg[1] * dims->nlg[2];        // istride = "jump for getting to next i index"
+        // ssize_t: predefined type of signed integer (= signed integral type) (eqvuivalent to Py_ssize_t in python, cython)
+        const ssize_t jstride = dims->nlg[2];                       // jstride = "jump for getting to next j index"
+
+        const ssize_t imin = 1;
+        const ssize_t jmin = 1;
+        const ssize_t kmin = 1;
+
+        const ssize_t imax = dims->nlg[0]-2;
+        const ssize_t jmax = dims->nlg[1]-2;
+        const ssize_t kmax = dims->nlg[2]-2;
+
+        const ssize_t stencil[3] = {istride,jstride,1};             // array, containing 3 elements
+        const ssize_t sp1_ed = stencil[d_advecting];
+        const ssize_t sp2_ed = 2 * sp1_ed ;
+        const ssize_t sm1_ed = -sp1_ed ;
+
+        const ssize_t sp1_ing = stencil[d_advected];                // d_advecting, d_advected: given as parameters to this function
+        const ssize_t sp2_ing = 2 * sp1_ing;
+        const ssize_t sm1_ing = -sp1_ing;
+
+
+
+        // (1) interpolate velocity fields
+        for(ssize_t i=imin;i<imax;i++){
+            const ssize_t ishift = i*istride;
+            for(ssize_t j=jmin;j<jmax;j++){
+                const ssize_t jshift = j*jstride;
+                for(ssize_t k=kmin;k<kmax;k++){
+                    const ssize_t ijk = ishift + jshift + k;
+                    vel_int_ing[ijk] = interp_4(vel_advecting[ijk+sm1_ing],vel_advecting[ijk],vel_advecting[ijk+sp1_ing],vel_advecting[ijk+sp2_ing]);
+                    vel_int_ed[ijk] = interp_4(vel_advected[ijk+sm1_ed],vel_advected[ijk],vel_advected[ijk+sp1_ed],vel_advected[ijk+sp2_ed]);
+                }
+            }
+        }
+
+
+        // (2) average interpolated velocity fields
+        //vel_mean_ing = Pa.HorizontalMean(Gr, &vel_int_advecting);
+        horizontal_mean_return(dims, &vel_int_ing[0], &vel_mean_ing[0]);
+        horizontal_mean_return(dims, &vel_int_ed[0], &vel_mean_ed[0]);
+
+
+        // (3) compute eddy flux: (vel - mean_vel)**2 AND compute total flux
+        if (d_advected != 2 && d_advecting !=2){                    // exclude w.u, w.v, w.w, u.w, v.w (advection by or of vertical velocity)
+            for(ssize_t i=imin;i<imax;i++){
+                const ssize_t ishift = i*istride;
+                for(ssize_t j=jmin;j<jmax;j++){
+                    const ssize_t jshift = j*jstride;
+                    for(ssize_t k=kmin;k<kmax;k++){
+                        const ssize_t ijk = ishift + jshift + k;
+                        eddy_flux[ijk] = (vel_int_ing[ijk] - vel_mean_ing[k]) * (vel_int_ed[ijk] - vel_mean_ed[k]) * rho0_half[k];
+                        flux[ijk] = (vel_int_ing[ijk] * vel_int_ed[ijk]) * rho0_half[k];
+                    }
+                }
+            }
+        }
+        else if(d_advected == 2 && d_advecting == 2){               // w.w
+            for(ssize_t i=imin;i<imax;i++){
+                const ssize_t ishift = i*istride;
+                for(ssize_t j=jmin;j<jmax;j++){
+                    const ssize_t jshift = j*jstride;
+                    for(ssize_t k=kmin;k<kmax;k++){
+                        const ssize_t ijk = ishift + jshift + k;
+                        eddy_flux[ijk] = (vel_int_ing[ijk] - vel_mean_ing[k]) * (vel_int_ed[ijk] - vel_mean_ed[k]) * rho0_half[k];
+                        flux[ijk] = (vel_int_ing[ijk] * vel_int_ed[ijk]) * rho0_half[k];
+                    }
+                }
+            }
+        }
+        else{
+            for(ssize_t i=imin;i<imax;i++){                         // u.w, v.w, w.u, w.v
+                const ssize_t ishift = i*istride;
+                for(ssize_t j=jmin;j<jmax;j++){
+                    const ssize_t jshift = j*jstride;
+                    for(ssize_t k=kmin;k<kmax;k++){
+                        const ssize_t ijk = ishift + jshift + k;
+                        eddy_flux[ijk] = (vel_int_ing[ijk] - vel_mean_ing[k]) * (vel_int_ed[ijk] - vel_mean_ed[k]) * rho0_half[k];
+                        flux[ijk] = (vel_int_ing[ijk] * vel_int_ed[ijk]) * rho0_half[k];
+                    }
+                }
+            }
+        }
+
+
+        // (4) compute mean eddy flux
+        horizontal_mean_return(dims, &eddy_flux[0], &mean_eddy_flux[0]);
+
+
+        // (5) compute QL flux: flux = flux - eddy_flux + mean_eddy_flux
+        for(ssize_t i=imin;i<imax;i++){
+            const ssize_t ishift = i*istride;
+            for(ssize_t j=jmin;j<jmax;j++){
+                const ssize_t jshift = j*jstride;
+                for(ssize_t k=kmin;k<kmax;k++){
+                    const ssize_t ijk = ishift + jshift + k;
+                    flux[ijk] = flux[ijk] - eddy_flux[ijk] + mean_eddy_flux[k];
+//                    flux[ijk] = flux[ijk];
+                }
+            }
+        }
+
+
+        momentum_flux_divergence(dims, alpha0, alpha0_half, flux,
+                                tendency, d_advected, d_advecting);
+
+        //Free dynamically allocated array
         free(eddy_flux);
         free(mean_eddy_flux);
         free(vel_int_ing);
