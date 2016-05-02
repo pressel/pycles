d7e44c2d648d990a218dd5f42d686e293c5371ec
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2016-05-02 15:43:08 +0200

    print statement

--- Csrc/momentum_advection_weno_ql.h
+++ Csrc/momentum_advection_weno_ql.h
@@ -401,9 +401,9 @@ void weno_fifth_order_m_decomp(struct DimStruct *dims, double* restrict rho0, do
                     const ssize_t jshift = j*jstride;
                     for(ssize_t k=kmin;k<kmax;k++){
                         const int ijk = ishift + jshift + k;
-                        if(fabs((flux[ijk]-flux_old[ijk])/flux_old[ijk])>0.01){count = count + 1;}
+//                        if(fabs((flux[ijk]-flux_old[ijk])/flux_old[ijk])>0.01){count = count + 1;}
 //                        if(fabs((flux[ijk]-flux_old[ijk])/flux_old[ijk])>0.01){printf("achtung, ijk= %d, diff = %f, flux_old = %f \n", ijk, flux[ijk]-flux_old[ijk], flux_old[ijk]);}
-//                        if(fabs((flux[ijk]-flux_old[ijk]))>fabs(flux_old[ijk])){printf("achtung, ijk= %d, diff = %f, flux_old = %f \n", ijk, flux[ijk]-flux_old[ijk], flux_old[ijk]);}
+                        if(fabs((flux[ijk]-flux_old[ijk]))>0.05*fabs(flux_old[ijk])){count = count + 1;}
 //                        if(fabs(flux_old[ijk])<1e-10){printf("zero old flux\n");}
                  }
              }
