b455cccb166ae1389f983ecef77d7a7bcd88e18d
Bettina Meyer <meyerbe@stud.phys.ethz.ch>
2016-05-06 11:06:03 +0200

    4th SA decomp: debugging (debugging Nr. 6; adding memory phi_fluc)

--- Csrc/scalar_advection_ql.h
+++ Csrc/scalar_advection_ql.h
@@ -138,7 +138,7 @@ void fourth_order_a_decomp(struct DimStruct *dims, double* restrict rho0, double
     if(d==2){printf("4th order Scalar Transport decomp \n");}
 
     double *vel_mean = (double *)malloc(sizeof(double) * dims->nlg[2]);
-//    double *phi_fluc = (double *)malloc(sizeof(double) * dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
+    double *phi_fluc = (double *)malloc(sizeof(double) * dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
     double *phi_mean = (double *)malloc(sizeof(double) * dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
     double *phi_mean_ = (double *)malloc(sizeof(double) * dims->nlg[2]);
 
@@ -265,7 +265,7 @@ void fourth_order_a_decomp(struct DimStruct *dims, double* restrict rho0, double
 //    free(mix_flux_phimean);
 //    free(mean_flux);
 //    free(flux_old);
-//    free(phi_fluc);
+    free(phi_fluc);
     free(phi_mean);
     free(phi_mean_);
     free(vel_mean);
