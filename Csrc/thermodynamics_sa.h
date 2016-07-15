2ef6a1192be415cc7cabab5064cf1d46c01f662e
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2016-07-15 14:56:04 +0200

    ThermoSA

--- Csrc/thermodynamics_sa.h
+++ Csrc/thermodynamics_sa.h
@@ -89,6 +89,8 @@ void eos_c_refstate(struct LookupStruct *LT, double (*lam_fp)(double), double (*
 
 void eos_c(struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(double, double),
                     const double p0, const double s, const double qt, double* T, double* qv, double* ql, double *qi){
+//    printf("doing saturation adjustment (eos_c)\n");
+
     *qv = qt;
     *ql = 0.0;
     *qi = 0.0;
@@ -98,15 +100,28 @@ void eos_c(struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(dou
     double pv_star_1 = lookup(LT, T_1);
     double qv_star_1 = qv_star_c(p0,qt,pv_star_1);
 
+//    printf("eos_c: qt = %f, qv_star_1 = %f, qv = %f\n", qt, qv_star_1, *qv);        // in initialisation: qt > qv_star_1 (qt ~ 10*qv_star_1)
+
     // If not saturated
     if(qt <= qv_star_1){
+        printf("eos_c: not saturated\n");
         *T = T_1;
+        // __
+        /*printf("no iteration\n");
+        double sigma_1 = qt - qv_star_1;
+        double lam_1 = lam_fp(T_1);         // = lambda(T_1)?
+        *ql = lam_1 * sigma_1;              // from paper: q_l,1 = lambda_1(T_1)*sigma_1
+                                            // *ql = lam_2 * sigma_2;
+        *qi = (1.0 - lam_1) * sigma_1;      // from paper: q_i,1 = [1-lambda(T_1)] * sigma_1
+                                            // *qi = (1.0 - lam_2) * sigma_2;*/
+        // __
         return;
     }
     else{
+//        printf("eos_c: saturated\n");
         double sigma_1 = qt - qv_star_1;
         double lam_1 = lam_fp(T_1);
-        double L_1 = L_fp(T_1,lam_1);
+        double L_1 = L_fp(T_1,lam_1);       // L_fp = ThermdynamicsSA.L_fp = Thermodynamics/LatentHeat.L_fp --> LatentHeat.L_fp = LatentHeat.L
         double s_1 = sd_c(pd_1,T_1) * (1.0 - qt) + sv_c(pv_1,T_1) * qt + sc_c(L_1,T_1)*sigma_1;
         double f_1 = s - s_1;
         double T_2 = T_1 + sigma_1 * L_1 /((1.0 - qt)*cpd + qv_star_1 * cpv);
@@ -114,6 +129,15 @@ void eos_c(struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(dou
         double qv_star_2;
         double sigma_2;
         double lam_2;
+        // __
+        int count = 0;
+        /*
+        int max_count = 0;
+        double pv_star_2 = lookup(LT, T_2);
+        qv_star_2 = qv_star_c(p0,qt,pv_star_2);
+        sigma_2 = qt - qv_star_2;
+        lam_2 = lam_fp(T_2);*/
+        // __
         do{
             double pv_star_2 = lookup(LT, T_2);
             qv_star_2 = qv_star_c(p0,qt,pv_star_2);
@@ -129,11 +153,20 @@ void eos_c(struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(dou
             T_2 = T_n;
             f_1 = f_2;
             delta_T  = fabs(T_2 - T_1);
+            count ++;
         } while(delta_T >= 1.0e-3 || sigma_2 < 0.0 );
+//        } while((delta_T >= 1.0e-3 || sigma_2 < 0.0) && count < 2);*/
         *T  = T_2;
         *qv = qv_star_2;
         *ql = lam_2 * sigma_2;
         *qi = (1.0 - lam_2) * sigma_2;
+        // __
+        printf("eos_c iterations: count = %d\n",count);
+        /*if(max_count < count){
+            max_count = count;
+            printf("max count = %d\n", max_count);}*/
+//        printf("ql = %f\n", *ql);
+        // __
         return;
     }
 }
