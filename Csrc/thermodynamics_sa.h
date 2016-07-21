04233c8e9efc88555f6e71a72fd7752264860584
Bettina Meyer <meyerbe@stud.phys.ethz.ch>
2016-07-21 15:21:05 +0200

    back: thermodynamics_sa.h (modifying eos_c_update)

--- Csrc/thermodynamics_sa.h
+++ Csrc/thermodynamics_sa.h
@@ -146,8 +146,14 @@ void eos_c(struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(dou
 void eos_update(struct DimStruct *dims, struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(double, double),
     double* restrict p0, double* restrict s, double* restrict qt, double* restrict T,
     double* restrict qv, double* restrict ql, double* restrict qi, double* restrict alpha ){
-
+//void eos_update(struct DimStruct *dims, struct LookupStruct *LT, double (*lam_fp)(double), double (*L_fp)(double, double),
+//    double* restrict p0, double* restrict s, double* restrict qt, double* restrict T,
+//    double* restrict qv, double* restrict ql, double* restrict qi, double* restrict alpha, int* n_nan ){
+//    printf("eos_update\n");
     ssize_t i,j,k;
+    // __
+    int i_,j_,k_;
+    // __
     const ssize_t istride = dims->nlg[1] * dims->nlg[2];
     const ssize_t jstride = dims->nlg[2];
     const ssize_t imin = 0;
@@ -157,7 +163,6 @@ void eos_update(struct DimStruct *dims, struct LookupStruct *LT, double (*lam_fp
     const ssize_t jmax = dims->nlg[1];
     const ssize_t kmax = dims->nlg[2];
 
-
     for (i=imin; i<imax; i++){
        const ssize_t ishift = i * istride;
         for (j=jmin;j<jmax;j++){
@@ -166,7 +171,6 @@ void eos_update(struct DimStruct *dims, struct LookupStruct *LT, double (*lam_fp
                     const ssize_t ijk = ishift + jshift + k;
                     eos_c(LT, lam_fp, L_fp, p0[k], s[ijk],qt[ijk],&T[ijk],&qv[ijk],&ql[ijk],&qi[ijk]);
                     alpha[ijk] = alpha_c(p0[k], T[ijk],qt[ijk],qv[ijk]);
-
                 } // End k loop
             } // End j loop
         } // End i loop
