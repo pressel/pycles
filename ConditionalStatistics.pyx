592a36c2dbc7a8bdfbaaa2a483f8ec46d7d95cc4
Bettina Meyer <meyerbe@stud.phys.ethz.ch>
2016-07-22 12:58:37 +0200

    back: ConditionalStatistics.pyx (adding NanStatistics class --> commented out)

--- ConditionalStatistics.pyx
+++ ConditionalStatistics.pyx
@@ -41,8 +41,14 @@ cdef class ConditionalStatistics:
             self.CondStatsClasses.append(SpectraStatistics(Gr,PV, DV, NC, Pa))
         if 'Null' in conditional_statistics:
             self.CondStatsClasses.append(NullCondStats())
-
-
+        # # __
+        # if 'NanStatistics' in conditional_statistics:
+        #     self.CondStatsClasses.append(NanStatistics(Gr, PV, DV, NC, Pa))
+        # # if 'Test' in conditional_statistics:
+        # #     self.CondStatsClasses.append(TestStatistics(Gr, PV, DV, NC, Pa))
+        # # __
+        #
+        # print('CondStatsClasses: ', self.CondStatsClasses)
         return
 
 
@@ -51,6 +57,7 @@ cdef class ConditionalStatistics:
 
         #loop over class instances and class stats_io
         for _class in self.CondStatsClasses:
+            # print('in stats_io:', _class)
             _class.stats_io(Gr, RS, PV, DV, NC, Pa)
 
         return
@@ -133,6 +140,17 @@ cdef class SpectraStatistics:
         self.Y_Pencil.initialize(Gr,Pa,dim=1)
 
 
+        # # _____
+        # Pa.root_print('NanStatistics initialization')
+        #
+        # self.sk_arr = np.zeros((1,2),dtype=np.double)
+        # self.qtk_arr = np.zeros((1,2),dtype=np.double)
+        #
+        # nz = np.arange(Gr.dims.n[2], dtype=np.double) * Gr.dims.dx[2]
+        # # NC.create_condstats_group('nan_array','nz', nz, Gr, Pa)
+        # # set up the names of the variables
+        # NC.add_condstat('sk_arr', 'spectra', 'wavenumber', Gr, Pa)
+        # NC.add_condstat('qtk_arr', 'spectra', 'wavenumber', Gr, Pa)
 
 
         return
@@ -373,4 +391,240 @@ cdef class SpectraStatistics:
                 spec[k, ik] = Pa.domain_scalar_sum(spec[k,ik])
 
         return spec
+
+#
+# # __________
+# cdef class NanStatistics:
+#     def __init__(self, Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV, DiagnosticVariables.DiagnosticVariables DV,
+#                  NetCDFIO_CondStats NC, ParallelMPI.ParallelMPI Pa):
+#         Pa.root_print('NanStatistics initialized')
+#         # cdef:
+#         #    Py_ssize_t nz = Gr.dims.n[2]
+#
+#         # self.sk_arr = np.zeros((1,2),dtype=np.double)
+#         # self.qtk_arr = np.zeros((1,2),dtype=np.double)
+#         self.sk_arr = np.zeros((Gr.dims.npd),dtype=np.double)
+#         self.qtk_arr = np.zeros((Gr.dims.npd),dtype=np.double)
+#
+#         # nz = np.arange(Gr.dims.n[2], dtype=np.double) * Gr.dims.dx[2]
+#         # NC.create_condstats_group('nan_array','nz', nz, Gr, Pa)
+#         nz = np.arange(Gr.dims.npd, dtype=np.double)
+#         NC.create_condstats_group('nan_array','nz',nz, Gr, Pa)
+#         # set up the names of the variables
+#         NC.add_condstat('sk_arr', 'nan_array', 'nz', Gr, Pa)
+#         NC.add_condstat('qtk_arr', 'nan_array', 'nz', Gr, Pa)
+#
+#
+#         ## from NetCDFIO_CondStats:
+#         # root_grp = nc.Dataset(self.path_plus_file, 'w', format='NETCDF4')
+#         # sub_grp = root_grp.createGroup(groupname)
+#         # sub_grp.createDimension('z', Gr.dims.n[2])
+#         # sub_grp.createDimension(dimname, len(dimval))
+#         # sub_grp.createDimension('t', None)
+#         # z = sub_grp.createVariable('z', 'f8', ('z'))
+#         # z[:] = np.array(Gr.z[Gr.dims.gw:-Gr.dims.gw])
+#         # dim = sub_grp.createVariable(dimname, 'f8', (dimname))
+#         # dim[:] = np.array(dimval[:])
+#         # sub_grp.createVariable('t', 'f8', ('t'))
+#
+#         return
+#
+#
+#     cpdef stats_io(self, Grid.Grid Gr, ReferenceState.ReferenceState RS, PrognosticVariables.PrognosticVariables PV,
+#                  DiagnosticVariables.DiagnosticVariables DV,  NetCDFIO_CondStats NC, ParallelMPI.ParallelMPI Pa):
+#
+#         Pa.root_print('!!!! calling ConditionalStatistics.NanStatistics stats_io')
+#
+#         # _____
+#         message = 'hi'
+#         print('sk_arr before:', self.sk_arr)
+#         self.nan_checking(message,Gr,PV,DV,NC,Pa)
+#
+#
+#         print('sk_arr after:', self.sk_arr)
+#         if 's' in PV.name_index:
+#             NC.write_condstat('sk_arr', 'nan_array', self.sk_arr[:,:], Pa)
+#         if 'qt' in PV.name_index:
+#             NC.write_condstat('qtk_arr', 'nan_array', self.qt_arr[:,:], Pa)
+#
+#         return
+#
+#
+#
+#     # def debug_tend(self,message):
+#     cpdef nan_checking(self,message, Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV,
+#                        DiagnosticVariables.DiagnosticVariables DV, NetCDFIO_CondStats NC, ParallelMPI.ParallelMPI Pa):
+#         print('nan_checking')
+#         cdef:
+#             PrognosticVariables.PrognosticVariables PV_ = PV
+#             DiagnosticVariables.DiagnosticVariables DV_ = DV
+#             Grid.Grid Gr_ = Gr
+#
+#         cdef:
+#             Py_ssize_t u_varshift = PV_.get_varshift(Gr,'u')
+#             Py_ssize_t v_varshift = PV_.get_varshift(Gr,'v')
+#             Py_ssize_t w_varshift = PV_.get_varshift(Gr,'w')
+#             Py_ssize_t s_varshift = PV_.get_varshift(Gr,'s')
+#
+#             Py_ssize_t istride = Gr_.dims.nlg[1] * Gr_.dims.nlg[2]
+#             Py_ssize_t jstride = Gr_.dims.nlg[2]
+#             Py_ssize_t imax = Gr_.dims.nlg[0]
+#             Py_ssize_t jmax = Gr_.dims.nlg[1]
+#             Py_ssize_t kmax = Gr_.dims.nlg[2]
+#             Py_ssize_t ijk_max = imax*istride + jmax*jstride + kmax
+#
+#             Py_ssize_t i, j, k, ijk, ishift, jshift
+#             Py_ssize_t imin = 0#Gr_.dims.gw
+#             Py_ssize_t jmin = 0#Gr_.dims.gw
+#             Py_ssize_t kmin = 0#Gr_.dims.gw
+#
+#         # __
+#         PV_.values[u_varshift+1] = np.nan
+#         # __
+#
+#         u_max = np.nanmax(PV_.tendencies[u_varshift:v_varshift])
+#         uk_max = np.nanargmax(PV_.tendencies[u_varshift:v_varshift])
+#         u_min = np.nanmin(PV_.tendencies[u_varshift:v_varshift])
+#         uk_min = np.nanargmin(PV_.tendencies[u_varshift:v_varshift])
+#         v_max = np.nanmax(PV_.tendencies[v_varshift:w_varshift])
+#         vk_max = np.nanargmax(PV_.tendencies[v_varshift:w_varshift])
+#         v_min = np.nanmin(PV_.tendencies[v_varshift:w_varshift])
+#         vk_min = np.nanargmin(PV_.tendencies[v_varshift:w_varshift])
+#         w_max = np.nanmax(PV_.tendencies[w_varshift:s_varshift])
+#         wk_max = np.nanargmax(PV_.tendencies[w_varshift:s_varshift])
+#         w_min = np.nanmin(PV_.tendencies[w_varshift:s_varshift])
+#         wk_min = np.nanargmin(PV_.tendencies[w_varshift:s_varshift])
+#
+#         u_nan = np.isnan(PV_.tendencies[u_varshift:v_varshift]).any()
+#         uk_nan = np.argmax(PV_.tendencies[u_varshift:v_varshift])
+#         v_nan = np.isnan(PV_.tendencies[v_varshift:w_varshift]).any()
+#         vk_nan = np.argmax(PV_.tendencies[v_varshift:w_varshift])
+#         w_nan = np.isnan(PV_.tendencies[w_varshift:s_varshift]).any()
+#         wk_nan = np.argmax(PV_.tendencies[w_varshift:s_varshift])
+#
+#         if Pa.rank == 0:
+#             print(message, 'debugging (max, min, nan): ')
+#             print('shifts', u_varshift, v_varshift, w_varshift, s_varshift)
+#             print('u tend: ', u_max, uk_max, u_min, uk_min, u_nan, uk_nan)
+#             print('v tend: ', v_max, vk_max, v_min, vk_min, v_nan, vk_nan)
+#             print('w tend: ', w_max, wk_max, w_min, wk_min, w_nan, wk_nan)
+#
+#         if 'qt' in PV_.name_index:
+#             qt_varshift = PV_.get_varshift(Gr,'qt')
+#             ql_varshift = DV_.get_varshift(Gr,'ql')
+#
+#             s_max = np.nanmax(PV_.tendencies[s_varshift:qt_varshift])
+#             sk_max = np.nanargmax(PV_.tendencies[s_varshift:qt_varshift])
+#             s_min = np.nanmin(PV_.tendencies[s_varshift:qt_varshift])
+#             sk_min = np.nanargmin(PV_.tendencies[s_varshift:qt_varshift])
+#             qt_max = np.nanmax(PV_.tendencies[qt_varshift:-1])
+#             qtk_max = np.nanargmax(PV_.tendencies[qt_varshift:-1])
+#             qt_min = np.nanmin(PV_.tendencies[qt_varshift:-1])
+#             qtk_min = np.nanargmin(PV_.tendencies[qt_varshift:-1])
+#
+#             s_nan = np.isnan(PV_.tendencies[s_varshift:qt_varshift]).any()
+#             sk_nan = np.argmax(PV_.tendencies[s_varshift:qt_varshift])
+#             qt_nan = np.isnan(PV_.tendencies[qt_varshift:-1]).any()
+#             qtk_nan = np.argmax(PV_.tendencies[qt_varshift:-1])
+#
+#             s_max_val= np.nanmax(PV_.values[s_varshift:qt_varshift])
+#             sk_max_val = np.nanargmax(PV_.values[s_varshift:qt_varshift])
+#             s_min_val = np.nanmin(PV_.values[s_varshift:qt_varshift])
+#             sk_min_val = np.nanargmin(PV_.tendencies[s_varshift:qt_varshift])
+#             s_nan_val = np.isnan(PV_.values[s_varshift:qt_varshift]).any()
+#             sk_nan_val = np.argmax(PV_.values[s_varshift:qt_varshift])
+#             qt_max_val = np.nanmax(PV_.values[qt_varshift:-1])
+#             qtk_max_val = np.nanargmax(PV_.values[qt_varshift:-1])
+#             qt_min_val = np.nanmin(PV_.values[qt_varshift:-1])
+#             if qt_min_val < 0:
+#                 Pa.root_print('qt val negative')
+#             qtk_min_val = np.nanargmin(PV_.values[qt_varshift:-1])
+#             qt_nan_val = np.isnan(PV_.values[qt_varshift:-1]).any()
+#             qtk_nan_val = np.argmax(PV_.values[qt_varshift:-1])
+#
+#             ql_max_val = np.nanmax(DV_.values[ql_varshift:(ql_varshift+ijk_max)])
+#             ql_min_val = np.nanmin(DV_.values[ql_varshift:(ql_varshift+ijk_max)])
+#             qlk_max_val = np.nanargmax(DV_.values[ql_varshift:(ql_varshift+ijk_max)])
+#             qlk_min_val = np.nanargmin(DV_.values[ql_varshift:(ql_varshift+ijk_max)])
+#             ql_nan_val = np.isnan(DV_.values[ql_varshift:(ql_varshift+ijk_max)]).any()
+#             qlk_nan_val = np.argmax(DV_.values[ql_varshift:(ql_varshift+ijk_max)])
+#
+#             if Pa.rank == 0:
+#                 print('s tend: ', s_max, sk_max, s_min, sk_min, s_nan, sk_nan)
+#                 print('s val: ', s_max_val, sk_max_val, s_min_val, sk_min_val, s_nan_val, sk_nan_val)
+#                 print('qt tend: ', qt_max, qtk_max, qt_min, qtk_min, qt_nan, qtk_nan)
+#                 print('qt val: ', qt_max_val, qtk_max_val, qt_min_val, qtk_min_val, qt_nan_val, qtk_nan_val)
+#                 print('ql val: ', ql_max_val, qlk_max_val, ql_min_val, qlk_min_val, ql_nan_val, qlk_nan_val)
+#
+#
+#         #for name in PV.name_index.keys():
+#             # with nogil:
+#             if 1 == 1:
+#                 for i in range(imin, imax):
+#                     ishift = i * istride
+#                     for j in range(jmin, jmax):
+#                         jshift = j * jstride
+#                         for k in range(kmin, kmax):
+#                             ijk = ishift + jshift + k
+#                             if np.isnan(PV_.values[s_varshift+ijk]):
+#                                 self.sk_arr = np.append(self.sk_arr,np.array([[ijk,k]]),axis=0)
+#                             if np.isnan(PV_.values[qt_varshift+ijk]):
+#                                 self.qtk_arr = np.append(self.qtk_arr,np.array([[ijk,k]]),axis=0)
+#             if np.size(self.sk_arr) > 1 or np.size(self.qtk_arr) > 1:
+#                 self.output_nan_array(Gr, PV, DV, NC, Pa)
+#             # if np.size(self.sk_arr) > 1:
+#             #     if self.Pa.rank == 0:
+#             #         print('sk_arr size: ', self.sk_arr.shape)
+#             #         print('sk_arr:', self.sk_arr)
+#             # if np.size(self.qtk_arr) > 1:
+#             #     if self.Pa.rank == 0:
+#             #         print('qtk_arr size: ', self.qtk_arr.shape)
+#             #         print('qtk_arr: ', self.qtk_arr)
+#
+#         else:
+#             s_max = np.nanmax(PV_.tendencies[s_varshift:-1])
+#             sk_max = np.nanargmax(PV_.tendencies[s_varshift:-1])
+#             s_min = np.nanmin(PV_.tendencies[s_varshift:-1])
+#             sk_min = np.nanargmin(PV_.tendencies[s_varshift:-1])
+#             s_nan = np.isnan(PV_.tendencies[s_varshift:-1]).any()
+#             sk_nan = np.argmax(PV_.tendencies[s_varshift:-1])
+#
+#             s_max_val= np.nanmax(PV_.values[s_varshift:-1])
+#             sk_max_val = np.nanargmax(PV_.values[s_varshift:-1])
+#             s_min_val = np.nanmin(PV_.values[s_varshift:-1])
+#             sk_min_val = np.nanargmin(PV_.tendencies[s_varshift:-1])
+#             s_nan_val = np.isnan(PV_.values[s_varshift:-1]).any()
+#             sk_nan_val = np.argmax(PV_.values[s_varshift:-1])
+#
+#             if Pa.rank == 0:
+#                 print('s tend: ', s_max, sk_max, s_min, sk_min, s_nan, sk_nan)
+#                 print('s val: ', s_max_val, sk_max_val, s_min_val, sk_min_val, s_nan_val, sk_nan_val)
+#
+#
+#             if 1 == 1:
+#                 for i in range(imin, imax):
+#                     ishift = i * istride
+#                     for j in range(jmin, jmax):
+#                         jshift = j * jstride
+#                         for k in range(kmin, kmax):
+#                             ijk = ishift + jshift + k
+#                             if np.isnan(PV_.values[s_varshift+ijk]):
+#                                 self.sk_arr = np.append(self.sk_arr,np.array([[ijk,k]]),axis=0)
+#             if np.size(self.sk_arr) > 1:
+#                 self.output_nan_array(Gr, PV, DV, NC, Pa)
+# #                 if self.Pa.rank == 0:
+# #                     print('sk_arr size: ', self.sk_arr.shape)
+# #                     print('sk_arr:', self.sk_arr)
+#         return
+#
+#
+#     cpdef output_nan_array(self, Grid.Grid Gr, PrognosticVariables.PrognosticVariables PV,
+#                  DiagnosticVariables.DiagnosticVariables DV,  NetCDFIO_CondStats NC, ParallelMPI.ParallelMPI Pa):
+#
+#         if 's' in PV.name_index:
+#             NC.write_condstat('sk_arr', 'nan_array', self.sk_arr[:,:], Pa)
+#         if 'qt' in PV.name_index:
+#             NC.write_condstat('qtk_arr', 'nan_array', self.qt_arr[:,:], Pa)
+#
+#         return
 
