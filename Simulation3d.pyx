0bc5909db3aa3879b4ae7dfa1e770b7949813644
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2016-08-09 12:02:59 +0200

    output T_max if w_tend = nan

--- Simulation3d.pyx
+++ Simulation3d.pyx
@@ -538,6 +538,13 @@ class Simulation3d:
             print('w tend: ', w_max, wk_max, w_min, wk_min, w_nan, wk_nan)
             print('w val: ', w_max_val, wk_max_val, w_min_val, wk_min_val, w_nan_val, wk_nan_val)
 
+            t_varshift = DV_.get_varshift(self.Gr,'temperature')
+            t_max_val = np.nanmax(DV_.values[t_varshift:t_varshift+ijk_max])
+            tk_max_val = np.nanargmax(DV_.values[t_varshift:t_varshift+ijk_max])
+            t_min_val = np.nanmin(DV_.values[t_varshift:t_varshift+ijk_max])
+            tk_min_val = np.nanargmin(DV_.values[t_varshift:t_varshift+ijk_max])
+            print('t val: ', t_max_val, tk_max_val, t_min_val, tk_min_val)
+
 
 
 
