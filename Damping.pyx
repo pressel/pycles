2fcadb67cb2db6c92ecd914ce2f66a34e1493fb6
pressel <presselk@ethz.ch>
2017-03-24 13:32:22 -0700

    Fixed error in damping.

--- Damping.pyx
+++ Damping.pyx
@@ -80,15 +80,19 @@ cdef class Rayleigh:
             dtype=np.double,
             order='c')
         self.gamma_z = np.zeros((Gr.dims.nlg[2]), dtype=np.double, order='c')
-        z_top = Gr.dims.dx[2] * Gr.dims.n[2]
+        z_top = Gr.zpl[Gr.dims.nlg[2] - Gr.dims.gw]
+
+        print z_top
         with nogil:
             for k in range(Gr.dims.nlg[2]):
-                if Gr.zl_half[k] >= z_top - self.z_d:
+                if Gr.zpl_half[k] >= z_top - self.z_d:
                     self.gamma_zhalf[
                         k] = self.gamma_r * sin((pi / 2.0) * (1.0 - (z_top - Gr.zpl_half[k]) / self.z_d))**2.0
-                if Gr.zl[k] >= z_top - self.z_d:
+                if Gr.zpl[k] >= z_top - self.z_d:
                     self.gamma_z[
                         k] = self.gamma_r * sin((pi / 2.0) * (1.0 - (z_top - Gr.zpl[k]) / self.z_d))**2.0
+
+
         return
 
     cpdef update(self, Grid.Grid Gr, ReferenceState.ReferenceState RS, PrognosticVariables.PrognosticVariables PV,
