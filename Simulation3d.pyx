654d36b499a4d233a2d8eb56a0579a3f2e7215f2
pressel <presselk@ethz.ch>
2017-03-28 10:06:46 -0700

    Added back radiation and surface budget computations to Simulation3d.

--- Simulation3d.pyx
+++ Simulation3d.pyx
@@ -170,8 +170,8 @@ class Simulation3d:
                 self.MD.update(self.Gr,self.Ref,self.PV,self.DV,self.Ke)
 
                 self.Fo.update(self.Gr, self.Ref, self.PV, self.DV, self.Pa)
-                #self.Ra.update(self.Gr, self.Ref, self.PV, self.DV, self.Sur, self.TS, self.Pa)
-                #self.Budg.update(self.Gr,self.Ra, self.Sur, self.TS, self.Pa)
+                self.Ra.update(self.Gr, self.Ref, self.PV, self.DV, self.Sur, self.TS, self.Pa)
+                self.Budg.update(self.Gr,self.Ra, self.Sur, self.TS, self.Pa)
                 #self.Tr.update_cleanup(self.Gr, self.Ref, PV_, DV_, self.Pa)
                 self.TS.update(self.Gr, self.PV, self.Pa)
                 PV_.Update_all_bcs(self.Gr, self.Pa)
