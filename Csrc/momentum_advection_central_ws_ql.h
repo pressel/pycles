378342e0a045c67d3f5efcdc51e67e4e47f5b653
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2017-07-23 07:27:00 +0200

    print statement

--- Csrc/momentum_advection_central_ws_ql.h
+++ Csrc/momentum_advection_central_ws_ql.h
@@ -11,8 +11,8 @@ void fourth_order_ws_m_ql(struct DimStruct *dims, double* restrict rho0, double*
     double* restrict vel_advected, double* restrict vel_advecting,
     double* restrict tendency, ssize_t d_advected, ssize_t d_advecting){
 
-        if (d_advected==1 && d_advecting==1){
-            printf("4th order WS QL Momentum Transport \n");}
+        //if (d_advected==1 && d_advecting==1){
+        //    printf("4th order WS QL Momentum Transport \n");}
 
         // Dynamically allocate flux array
         double *flux = (double *)malloc(sizeof(double)*dims->nlg[0] * dims->nlg[1] * dims->nlg[2]);
